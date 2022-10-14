0a38fbee3ecb76222336c944ac9f0175b050cde0
Viktor Diakonov <vip3352@gmail.com>
2022-10-14 14:40:28 +0300

    DZ23. Flyway добавил, проект работает

---
+++ src/main/resources/db.migration/V1__init.sql
@@ -0,0 +1,12 @@
+create table public.users (
+                              id integer primary key not null default nextval('users_id_seq1'::regclass),
+                              country character varying(255),
+                              email character varying(255),
+                              name character varying(255),
+                              address character varying,
+                              phone character varying,
+                              employee_id integer,
+                              foreign key (employee_id) references public.cards (id)
+                                  match simple on update no action on delete no action
+);
+
