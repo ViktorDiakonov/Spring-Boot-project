6a439c6ea0f770faf159ce8c52eb6603156d74e9
Viktor Diakonov <vip3352@gmail.com>
2022-10-28 16:44:12 +0300

    ДЗ 24. Spring Security - small fix finish

--- src/main/java/com/example/demowithtests/util/config/SecurityConfig.java
+++ src/main/java/com/example/demowithtests/util/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.demowithtests.service.ServiceBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -23,6 +24,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(serviceBean);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 
 
